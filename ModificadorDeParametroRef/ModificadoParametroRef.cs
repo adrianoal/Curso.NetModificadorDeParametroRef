62573c1e406b650ad4ef6c644ebafff4fb448c31
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-11 12:09:30 -0300

    Exemplo com mofificador de parametro REF

---
+++ ModificadorDeParametroRef/ModificadoParametroRef.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadorDeParametroRef
+{
+    class ModificadoParametroRef
+    {
+        /*
+        //--------------------------------------------------------\\
+        // Este metodo não funciona:
+        // Esta solucao abaixo, esta dentro do escopo, por isso no programa principal nao funciona. 
+
+        public static void Triple(int x) // Sem o Modificador Ref
+        {
+            x = x * 3;
+        }
+        //--------------------------------------------------------\\
+        */
+
+        // Este metodo funciona:
+        // Ref faz referencia a variavel principal (Definida no Man)
+        public static void Triple(ref int x) // Com o Modificador Ref
+        {
+            x = x * 3;
+        }
+    }
+}
---
+++ ModificadorDeParametroRef/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ModificadorDeParametroRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            ModificadoParametroRef.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
